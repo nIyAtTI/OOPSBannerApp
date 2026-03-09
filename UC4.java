1b39200ab30c1bc55732edec6e652f21be699a5b
Niyatti <nb2483@srmist.edu.in>
2026-03-10 03:10:17 +0530

    Coded UC4 to Print OOPS Banner to Console

---
+++ UC4.java
@@ -0,0 +1,12 @@
+public class UC3 {
+    public static void main(String[] args) {
+        String[] lines = new String[4];
+        line[0]=System.out.println(String.join("  *** "  ,  "  **  ",  "**  **", " **"));
+        line[1]=System.out.println(String.join("**   ** " ," **   **"," **  **","** "));
+        line[2]=System.out.println(String.join("**   ** " ," **   **", "**",  "   **"));  
+        line[3]=System.out.println(String.join("  ***  "    ," ** " ,  "** ", " **"));
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}    
