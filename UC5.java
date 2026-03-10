2c96dc4f8af691c243a96d56295d48d5c42a519d
Niyatti <nb2483@srmist.edu.in>
2026-03-10 12:49:09 +0530

    Coded UC5 to Print OOPS Banner to Console

---
+++ UC5.java
@@ -0,0 +1,14 @@
+public class UC3 {
+    public static void main(String[] args) {
+        String[] lines = new String[4];
+        String[]={
+        String.join("  *** "  ,  "  **  ",  "**  **", " **"));
+        String.join("**   ** " ," **   **"," **  **","** ")
+        String.join("**   ** " ," **   **", "**",  "   **"));  
+        String.join("  ***  "    ," ** " ,  "** ", " **"));
+        }
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}    
